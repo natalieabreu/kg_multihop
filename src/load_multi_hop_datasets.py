3027ad5a75e9750ce11ca3f95869a5e84684537b
Annabelle Carrell <belle.carrell@gmail.com>
2024-05-01 14:18:18 -0400

    Update dataset aggregation. Add train/test split to two hop.

--- src/load_multi_hop_datasets.py
+++ src/load_multi_hop_datasets.py
@@ -1,6 +1,10 @@
-from wikidata.utils import load_json
+from wikidata.utils import load_json, write_json
 import glob
 from query import Query
+import random
+
+seed = 12345
+random.seed(seed)
 
 two_hop = {}
 one_hop = {}
@@ -15,6 +19,28 @@ for fname in glob.glob('/n/holyscratch01/kempner_lab/Everyone/data/twohop/top_en
     #     partial[uuid] = Query.from_dict(dicts[uuid])
     # two_hop.update(partial)
 
+facts_str = list(two_hop.keys()) 
+print(f'{len(facts_str)} two hop facts')
+write_json(two_hop,'/n/holyscratch01/kempner_lab/Everyone/data/multihop/two_hop_dataset.json')
+
+facts_str = facts_str[:41000]
+train_facts = facts_str[:32800]
+test_facts = facts_str[32800:]
+
+train = {}
+test = {}
+for f in train_facts:
+    train[f] = two_hop[f]
+
+for f in test_facts:
+    test[f] = two_hop[f]
+
+print(f'{len(train_facts)} train facts')
+print(f'{len(test_facts)} test facts')
+
+write_json(train,'/n/holyscratch01/kempner_lab/Everyone/data/multihop/train_two_hop_dataset.json')
+write_json(test,'/n/holyscratch01/kempner_lab/Everyone/data/multihop/test_two_hop_dataset.json')
+
 for fname in glob.glob('/n/holyscratch01/kempner_lab/Everyone/data/twohop/top_ents_5000_entities_0_facts_each_5000_facts/one_hop*'):
     # Load facts as dicts
     one_hop.update(load_json(fname))
@@ -25,3 +51,6 @@ for fname in glob.glob('/n/holyscratch01/kempner_lab/Everyone/data/twohop/top_en
     # for uuid in dicts.keys():
     #     partial[uuid] = Query.from_dict(dicts[uuid])
     # one_hop.update(partial)
+
+print(f'{len(one_hop.keys())} one hop facts')
+write_json(one_hop,'/n/holyscratch01/kempner_lab/Everyone/data/multihop/one_hop_dataset.json')
